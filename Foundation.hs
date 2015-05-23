e15422e18f58c4d79b4da4ecbd10f791f196255c
Michal Blotniak <buoto@buzytor.com>
2015-05-23 15:03:31 +0200

    Add yesod files

---
+++ Foundation.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE ViewPatterns      #-}
+module Foundation where
+
+import Yesod.Core
+
+data App = App
+
+mkYesodData "App" $(parseRoutesFile "routes")
+
+instance Yesod App where
+    makeSessionBackend _ = return Nothing
