0866a6dbb00e1b978c490bafc69f61c9bb19918f
Michal Blotniak <buoto@buzytor.com>
2015-05-23 22:42:48 +0200

    Fix cors

--- Main.hs
+++ Main.hs
@@ -1,6 +1,11 @@
 import Application () -- for YesodDispatch instance
 import Import
+import Network.Wai
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.Cors
 import Yesod.Core
 
 main :: IO ()
-main = warp 4000 App
+main = do
+     waiApp <- toWaiApp App
+     run 4000 $ simpleCors waiApp
