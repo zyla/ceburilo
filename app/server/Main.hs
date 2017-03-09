7d6926a3949d7f2dd8a1f4244059eb461313815f
Przemysław Kopański <P.Kopanski@stud.elka.pw.edu.pl>
2017-03-09 16:15:05 +0100

    Migration to lts-8.4

--- app/server/Main.hs
+++ app/server/Main.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DataKinds, TypeOperators, RecordWildCards, OverloadedStrings #-}
 module Main where
 
-import Control.Monad.Trans.Either
+import Control.Monad.Trans.Except
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.Cors (simpleCors)
@@ -65,12 +65,12 @@ generateRouteView begPoint dstPoint graph spath =
 parseInput :: Graph -> IMap
            -> Maybe Float -> Maybe Float
            -> Maybe Float -> Maybe Float
-           -> EitherT ServantErr IO RouteView
+           -> ExceptT ServantErr IO RouteView
 parseInput g mp (Just blat) (Just blon) (Just dlat) (Just dlon) =
   case generateRouteView (Point blat blon) (Point dlat dlon) g mp of
-    Nothing -> left err500
+    Nothing -> throwE err500
     (Just x) -> return x
-parseInput _ _ _ _ _ _ = left err400
+parseInput _ _ _ _ _ _ = throwE err400
 
 
 
